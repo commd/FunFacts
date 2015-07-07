333548b8545dd2f69c5fc523f75dac0cc171f9dd
Chuck Huie <vidscmd@netscape.net>
2015-07-06 17:56:17 -0700

    Changes up to changing background colors.

---
+++ app/src/main/java/com/chuck/funfacts/FactBook.java
@@ -0,0 +1,32 @@
+package com.chuck.funfacts;
+
+import java.util.Random;
+
+/**
+ * Created by chuck on 7/6/15.
+ */
+public class FactBook {
+
+    // Member variables (properties about the object)
+    public String[] mfacts = {
+            "Ants stretch when they wake up in the morning.",
+            "Ostriches can run faster than horses.",
+            "Olympic gold medals are actually made mostly of silver.",
+            "You are born with 300 bones; by the time you are an adult you will have 206.",
+            "It takes about 8 minutes for light from the Sun to reach Earth.",
+            "Some bamboo plants can grow almost a meter in just one day.",
+            "The state of Florida is bigger than England.",
+            "Some penguins can leap 2-3 meters out of the water.",
+            "On average, it takes 66 days to form a new habit.",
+            "Mammoths still walked the earth when the Great Pyramid was being built." };
+
+
+    // Method (abiliteis: things the object can do
+    public String getFact() {
+        // Randomly select a fact
+        Random randomGenerator = new Random();  // Construct a new Random number generator
+        int randomNumber = randomGenerator.nextInt(mfacts.length);
+        String fact = mfacts[randomNumber];
+        return fact;
+    }
+}
